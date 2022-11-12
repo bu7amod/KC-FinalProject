a93d8b65f48db6f025d772bdb20e991f18b8e428
bu7amod <114930945+bu7amod@users.noreply.github.com>
2022-11-12 17:01:43 +0300

    gg

--- My project (1)/Assets/level 1/camera.cs
+++ My project (1)/Assets/level 1/camera.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    float move;
-
+    public float speed;
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    
         float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+    }
+
+    void FixedUpdate()
+    {
+
     }
 }
